d4f9b8946a6985b49ca8f73e2e582cbc06a31a7a
Marcel Raas <marcel.raas@gmail.com>
2014-02-15 14:42:16 +0100

    Add SortCache class.

---
+++ SortCache.h
@@ -0,0 +1,32 @@
+#ifndef SORTCACHE_H
+#define SORTCACHE_H
+
+#include "RealVector.h"
+
+/**
+ * @class SortCache
+ * @brief Class that provides indices to sorted variables. Helper class to loop over unsorted vectors such that they
+ * appear to be sorted. The index of the loop can be translated using the getSortedIndex method.
+ */
+class SortCache
+{
+   public:
+      /**
+       * Create a SortCache based on @param Values.
+       */
+      SortCache( const RealVector& values );
+      /**
+       * Destructor
+       */
+      virtual ~SortCache();
+
+      /**
+       * Get the index in the original vector of the @index-th item sorted by value.
+       */
+      size_t getSortedIndex( size_t index ) const;
+
+   private:
+      std::vector< size_t >   m_translation;
+};
+
+#endif // SORTCACHE_H
