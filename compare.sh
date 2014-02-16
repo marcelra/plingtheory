41cd0af360bdbc6d5c18362b93bab65f01eeb3f2
Marcel Raas <marcel.raas@gmail.com>
2014-02-16 20:38:54 +0100

    Change updates -> update in compare output file names.

--- compare.sh
+++ compare.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-./plingtheory testall -b -o updates.out -r updates.root -v 6
-diffuse updates.out ref.out 
-./plingtheory compare ref.root updates.root 
+./plingtheory testall -b -o update.out -r update.root -v 6
+diffuse update.out ref.out 
+./plingtheory compare ref.root update.root 
