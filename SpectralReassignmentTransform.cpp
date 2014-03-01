cbcf30eee5dee84a61a247cd1b5dff0083872fab
Marcel Raas <marcel.raas@gmail.com>
2014-03-01 01:00:19 +0100

    Little improvements in documentation and access of Stft results.

--- SpectralReassignmentTransform.cpp
+++ SpectralReassignmentTransform.cpp
@@ -1,35 +1,44 @@
 #include "SpectralReassignmentTransform.h"
 
 #include "Logger.h"
-
 #include "WindowFuncDef.h"
 
 namespace WaveAnalysis
 {
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// constructor
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 SpectralReassignmentTransform::SpectralReassignmentTransform( const SamplingInfo& samplingInfo, size_t fourierSize, size_t numSamplesZeroPadding, double hopRate ) :
    m_stft( samplingInfo, fourierSize, HanningWindowFuncDef(), numSamplesZeroPadding, hopRate ),
    m_stftDerivative( samplingInfo, fourierSize, HanningDerivativeWindowFuncDef(), numSamplesZeroPadding, hopRate ),
    m_stftTimeRamped( samplingInfo, fourierSize, HanningTimeRampedWindowFuncDef(), numSamplesZeroPadding, hopRate )
-{
-}
+{}
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// execute
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 StftData::Ptr SpectralReassignmentTransform::execute( const RawPcmData& data )
 {
    Logger msg( "SpectralReassignmentTransform" );
    msg << Msg::Verbose << "In execute..." << Msg::EndReq;
 
+   /// Create result object container.
+   StftData* result = new StftData( m_stft.getConfig() );
+
    msg << Msg::Verbose << "Executing the STFT transforms..." << Msg::EndReq;
+
+   /// Calculate the STFTs for the different Windows (ordinary, derivative and time-ramped).
    StftData::Ptr stft = m_stft.execute( data );
    StftData::Ptr stftDerivative = m_stftDerivative.execute( data );
    StftData::Ptr stftTimeRamped = m_stftTimeRamped.execute( data );
 
    assert( stft->getNumSpectra() == stftDerivative->getNumSpectra() );
    assert( stft->getNumSpectra() == stftTimeRamped->getNumSpectra() );
-   StftData* result = new StftData( m_stft.getConfig() );
 
    msg << Msg::Verbose << "Calculating the reassigned spectra." << Msg::EndReq;
 
+   /// Create SrSpectrum objects from the three spectra. The SrSpectrum class will calculate the actual omega_hat and t_hat.
    for ( size_t iSpec = 0; iSpec < stft->getNumSpectra(); ++iSpec )
    {
       const FourierSpectrum& ft = stft->getSpectrum( iSpec );
@@ -39,6 +48,8 @@ StftData::Ptr SpectralReassignmentTransform::execute( const RawPcmData& data )
    }
 
    msg << Msg::Verbose << "Done" << Msg::EndReq;
+
+   /// Wrap pointer.
    return StftData::Ptr( result );
 }
 
--- SpectralReassignmentTransform.h
+++ SpectralReassignmentTransform.h
@@ -10,17 +10,28 @@ class RawPcmData;
 namespace WaveAnalysis
 {
 
+/**
+ * @class SpectralReassignmentTransform
+ * @brief Calculates the Fourier trasnforms and performs the spectral reassigment method. Only the Hanning window is supported.
+ */
 class SpectralReassignmentTransform
 {
    public:
+      /**
+       * Constructor. Similar to StftAlgorithm, @see StftAlgorithm for more details.
+       */
       SpectralReassignmentTransform( const SamplingInfo& samplingInfo, size_t fourierSize, size_t numSamplesZeroPadding, double hopRate );
 
+      /**
+       * Execute the transform on data. For the Spectral reassigned transforms it is possible to query the SrSpectrum results
+       * from the StftData.
+       */
       StftData::Ptr execute( const RawPcmData& data );
 
    private:
-      StftAlgorithm     m_stft;
-      StftAlgorithm     m_stftDerivative;
-      StftAlgorithm     m_stftTimeRamped;
+      StftAlgorithm     m_stft;                 //! Ordinary Fourier transform
+      StftAlgorithm     m_stftDerivative;       //! Window derivate-transform
+      StftAlgorithm     m_stftTimeRamped;       //! Time-ramped transform
 };
 
 } /// namespace WaveAnalysis
