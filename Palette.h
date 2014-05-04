a35c6def026063956045481fbe99d7667d8373f5
Marcel Raas <marcel.raas@gmail.com>
2014-05-04 23:19:45 +0200

    Fix bug in Palette.

--- Palette.h
+++ Palette.h
@@ -6,13 +6,26 @@
 namespace Plotting
 {
 
+/**
+ * @class Palette
+ * @brief Class describing a colour palette. The values in getColour should be normalised to [0,1].
+ */
 class Palette
 {
    public:
+      /**
+       * Create a palette. At least two stops should be given at 0 and at 1.
+       */
       Palette( const std::vector< QColor >& stops, const std::vector< double >& stopPoints );
 
+      /**
+       * Obtain colour from value z. Z should be normalised between 0 and 1.
+       */
       QColor getColour( double z ) const;
 
+      /**
+       * Prefab palettes.
+       */
       static Palette whiteBlackPalette();
       static Palette blueRedPalette();
       static Palette heatPalette();
@@ -21,6 +34,9 @@ class Palette
       static Palette inverseRainbowPalette();
 
    private:
+      /**
+       * Morph color @param c1 to color @param c2. The parameter @param x should be between 0 and 1.
+       */
       QColor interpolate( const QColor& c1, const QColor& c2, double x ) const;
 
    private:
