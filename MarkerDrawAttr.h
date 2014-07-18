b4fcf67bd63bda615612e4c7fb4d9d736d943d8b
Marcel Raas <marcel.raas@gmail.com>
2014-07-18 17:56:45 +0200

    Set default marker to '+'.

--- MarkerDrawAttr.h
+++ MarkerDrawAttr.h
@@ -17,7 +17,7 @@ enum MarkerType
 class MarkerDrawAttr : public LineDrawAttr
 {
    public:
-      MarkerDrawAttr( QColor color = Qt::black, MarkerType markerType = MarkerRectangle, double markerSize = 3, double width = 2, bool antialiasing = true );
+      MarkerDrawAttr( QColor color = Qt::black, MarkerType markerType = MarkerPlus, double markerSize = 3, double width = 2, bool antialiasing = true );
 
       void setMarkerType( Plotting::MarkerType markerType );
       void setMarkerSize( double markerSize );
